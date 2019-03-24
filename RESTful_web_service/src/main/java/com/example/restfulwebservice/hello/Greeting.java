582ea7dd287930eafecd2acdb20de91ef9968976
wonderqin <qin_haida2017@163.com>
2019-03-24 16:48:15 +0800

    fix: only support the get method to the attribute.

--- RESTful_web_service/src/main/java/com/example/restfulwebservice/hello/Greeting.java
+++ RESTful_web_service/src/main/java/com/example/restfulwebservice/hello/Greeting.java
@@ -22,15 +22,7 @@ public class Greeting {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getContent() {
         return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 }
