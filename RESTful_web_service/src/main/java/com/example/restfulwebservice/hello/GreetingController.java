4063fc02c9f8f1bba497db0fd19b2d0efd785fc4
wonderqin <qin_haida2017@163.com>
2019-03-24 16:47:16 +0800

    fix:overwrite the static name occording to alibaba's java statute.

--- RESTful_web_service/src/main/java/com/example/restfulwebservice/hello/GreetingController.java
+++ RESTful_web_service/src/main/java/com/example/restfulwebservice/hello/GreetingController.java
@@ -14,11 +14,10 @@ import java.util.concurrent.atomic.AtomicLong;
  **/
 @RestController
 public class GreetingController {
-    private static final String template = "hello,%s";
+    private static final String TEMPLATE = "hello,%s";
     private final AtomicLong counter = new AtomicLong();
-
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name",defaultValue = "World") String name){
-        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+        return new Greeting(counter.incrementAndGet(),String.format(TEMPLATE,name));
     }
 }
