e077d80495108e08113c95e8c50351d8235b0a64
wonderqin <qin_haida2017@163.com>
2019-06-25 00:51:55 +0800

    update:更改该类为普通启动类

--- consumingrestfulwebservice/src/main/java/com/example/demo/ConsumingRestfulWebserviceApplication.java
+++ consumingrestfulwebservice/src/main/java/com/example/demo/ConsumingRestfulWebserviceApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
  * @Author wonderqin
  * @Description TODO
 **/
-@SpringBootApplication
 public class ConsumingRestfulWebserviceApplication {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ConsumingRestfulWebserviceApplication.class);
