1b9f88b34b6fc74648cf82b6a8ad2d5119667ef7
wonderqin <qin_haida2017@163.com>
2019-06-25 00:52:19 +0800

    add:新增springboot启动类

---
+++ consumingrestfulwebservice/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import com.example.demo.hello.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author wonderQin
+ * @Date 2019-06-25 0:30
+ **/
+@SpringBootApplication
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.info(quote.toString());
+        };
+    }
+}
