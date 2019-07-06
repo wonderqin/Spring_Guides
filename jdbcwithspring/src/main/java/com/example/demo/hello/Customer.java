e061e98afea885df32cb25403401ac13b8110e0d
wonderqin <qin_haida2017@163.com>
2019-07-06 23:47:57 +0800

    add:新增module:authenticating_ldap

--- jdbcwithspring/src/main/java/com/example/demo/hello/Customer.java
+++ jdbcwithspring/src/main/java/com/example/demo/hello/Customer.java
@@ -1,54 +1,17 @@
 package com.example.demo.hello;
 
+import lombok.*;
+
 /**
  * @ClassName Customer
  * @Description TODO
  * @Author wonderQin
  * @Date 2019-04-02 23:16
  **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     private long id;
     private String firstName,lastName;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Customer(long id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public Customer() {
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 }
